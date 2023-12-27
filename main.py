d024c0c4b63c38cbdf9599a2ac25b06c20945e7d
ildarghc <ildarpochta@mail.ru>
2023-12-28 03:42:01 +0500

    Строки и индексация строк

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 my_string = "мои стринги"
-my_number = 1
-print(my_string, "|", my_number)
+print(my_string[0])
+print(my_string[-1])
+print(my_string[2:4])
+print(len(my_string))
+another_string = 'другие стринги'
+new_string = my_string + ' и ' + another_string
+print(new_string)
