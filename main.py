ac69a56b12ff958748edbfdc6f230c684277ac59
ildarghc <ildarpochta@mail.ru>
2023-12-29 02:31:43 +0500

    "Переменные. Работа. в iPython"

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-my_string = "мои стринги"
-print(my_string[0])
-print(my_string[-1])
-print(my_string[2:4])
-print(len(my_string))
-another_string = 'другие стринги'
-new_string = my_string + ' и ' + another_string
-print(new_string)
+num1, num2 = 5, 3
+sum = num1 + num2
+print(sum)
+result = num1 * num2 + sum
+print(result)
