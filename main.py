5f5c6e06c8b2faf5b6024ca54761e0edf9015af9
ildarghc <ildarpochta@mail.ru>
2024-01-05 01:35:37 +0500

    "Неизменяемые и изменяемые объекты. Кортежи"

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-my_string = "ЭТО СТРИНГИ!"
-print(len(my_string))
-print(my_string.upper())
-print(my_string.lower())
-print(my_string.replace(' ', ''))
-print(my_string[0])
-print(my_string[-1])
+immutable_var = (1, 0.5, 'i')
+print(immutable_var)
+# Значения элементов кортежа нельзя изменить по определению, т.к. кортеж предполагает список, который нельзя изменить
+mutable_list = [1, 0.5, 'i']
+mutable_list.append('mutator')
+print(mutable_list)
